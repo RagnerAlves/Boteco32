cbdb8452cc1697ffa782dd3e5e9e7dadac6d96fa
Marcelo Nunes <fernandesnunes026@gmail.com>
2021-12-09 16:39:48 -0300

    Foi feito os ajustes no getClientes

--- Boteco32/Controllers/ClientesController.cs
+++ Boteco32/Controllers/ClientesController.cs
@@ -26,116 +26,127 @@ namespace Boteco32.Controllers
 
         // GET: api/Clientes teste 
         [HttpGet]
-        public IEnumerable<Cliente> GetClientes()
-        {       
-            return  _clienteService.BuscarClientes().ToList();
-        }
-
-        /*
-        // GET: api/Clientes/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Cliente>> GetCliente(int id)
+        public async Task<IActionResult> GetClientes()
         {
-            var cliente = await _context.Clientes.FindAsync(id);
-
-            if (cliente == null)
-            {
-                return NotFound();
-            }
-
-            return cliente;
-        }
-
-        // PUT: api/Clientes/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutCliente(int id, Cliente cliente)
-        {
-            if (id != cliente.Id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(cliente).State = EntityState.Modified;
-
             try
             {
-                await _context.SaveChangesAsync();
+                var listaDeClientes = _clienteService.BuscarClientes().ToList();
+                if (listaDeClientes == null)
+                {
+                    return NotFound(new RetornoViewModel<Cliente>("Nenhum cliente na base de dados"));
+                }
+                return Ok(new RetornoViewModel<List<Cliente>>(listaDeClientes));
             }
-            catch (DbUpdateConcurrencyException)
+            catch (Exception e)
             {
-                if (!ClienteExists(id))
+                return StatusCode(500, new RetornoViewModel<List<Cliente>>("Erro interno."));
+            }
+        } 
+            /*
+            // GET: api/Clientes/5
+            [HttpGet("{id}")]
+            public async Task<ActionResult<Cliente>> GetCliente(int id)
+            {
+                var cliente = await _context.Clientes.FindAsync(id);
+
+                if (cliente == null)
                 {
                     return NotFound();
                 }
-                else
+
+                return cliente;
+            }
+
+            // PUT: api/Clientes/5
+            // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+            [HttpPut("{id}")]
+            public async Task<IActionResult> PutCliente(int id, Cliente cliente)
+            {
+                if (id != cliente.Id)
                 {
-                    throw;
+                    return BadRequest();
                 }
-            }
 
-            return NoContent();
-        }
+                _context.Entry(cliente).State = EntityState.Modified;
 
-        // DELETE: api/Clientes/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteCliente(int id)
-        {
-            var cliente = await _context.Clientes.FindAsync(id);
-            if (cliente == null)
-            {
-                return NotFound();
-            }
-
-            _context.Clientes.Remove(cliente);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
-        }
-
-        private bool ClienteExists(int id)
-        {
-            return _context.Clientes.Any(e => e.Id == id);
-        }
-
-         */
-
-        // POST: api/Clientes
-        [HttpPost]
-        public async Task<ActionResult<Cliente>> PostCliente(
-                    [FromBody] CadastrarClienteViewModel clienteViewModel)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(new RetornoViewModel<Cliente>(ModelState.RecuperarErros()));
-            }
-            try
-            {
-                Cliente cliente = new Cliente()
+                try
                 {
-                    Id = 0,
-                    Codigo = clienteViewModel.Codigo,
-                    Nome = clienteViewModel.Nome,
-                    Endereco = clienteViewModel.Endereco,
-                    Pedidos = null
-                };
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ClienteExists(id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
 
-                await _clienteService.Adicionar(cliente);
-            
-                //return Created($"/{aluno.Id}", aluno);
-                return Created($"/{cliente.Id}", new RetornoViewModel<Cliente>(cliente));
-            }
-            catch (DbUpdateException ex)
-            {
-                //return StatusCode(500, "Falha ao atualizar o registro");
-                return StatusCode(500, new RetornoViewModel<List<Cliente>>("Falha ao atualizar o registro"));
-            }
-            catch (Exception ex)
-            {
-                //return StatusCode(500, "Erro interno");
-                return StatusCode(500, new RetornoViewModel<List<Cliente>>("Erro interno"));
+                return NoContent();
             }
 
+            // DELETE: api/Clientes/5
+            [HttpDelete("{id}")]
+            public async Task<IActionResult> DeleteCliente(int id)
+            {
+                var cliente = await _context.Clientes.FindAsync(id);
+                if (cliente == null)
+                {
+                    return NotFound();
+                }
+
+                _context.Clientes.Remove(cliente);
+                await _context.SaveChangesAsync();
+
+                return NoContent();
+            }
+
+            private bool ClienteExists(int id)
+            {
+                return _context.Clientes.Any(e => e.Id == id);
+            }
+
+             */
+
+            // POST: api/Clientes
+            [HttpPost]
+            public async Task<ActionResult<Cliente>> PostCliente(
+                                [FromBody] CadastrarClienteViewModel clienteViewModel)
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(new RetornoViewModel<Cliente>(ModelState.RecuperarErros()));
+                }
+                try
+                {
+                    Cliente cliente = new Cliente()
+                    {
+                        Id = 0,
+                        Codigo = clienteViewModel.Codigo,
+                        Nome = clienteViewModel.Nome,
+                        Endereco = clienteViewModel.Endereco,
+                        Pedidos = null
+                    };
+
+                    await _clienteService.Adicionar(cliente);
+
+                    //return Created($"/{aluno.Id}", aluno);
+                    return Created($"/{cliente.Id}", new RetornoViewModel<Cliente>(cliente));
+                }
+                catch (DbUpdateException ex)
+                {
+                    //return StatusCode(500, "Falha ao atualizar o registro");
+                    return StatusCode(500, new RetornoViewModel<List<Cliente>>("Falha ao atualizar o registro"));
+                }
+                catch (Exception ex)
+                {
+                    //return StatusCode(500, "Erro interno");
+                    return StatusCode(500, new RetornoViewModel<List<Cliente>>("Erro interno"));
+                }
+
+            }
         }
     }
-}
