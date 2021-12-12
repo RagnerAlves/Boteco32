199f80aaea3a4267f06eb49b5ac058416f29c45d
RicardoFreis <ricreis71@gmail.com>
2021-12-12 20:14:08 -0300

    Criação da ApiAutenticada e geração de Token.

---
+++ Boteco32/ApiAutenticada/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiQueExigeAutenticacao.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("")]
+        public IActionResult Get()
+        {
+            return Ok(new
+            {
+                Mensagem = "API com autenticação rodando com sucesso !"
+            }); 
+        }
+    }
+}
