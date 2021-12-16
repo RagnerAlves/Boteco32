603cec8c0e1e07dd1396047943d107c5d10ee190
RicardoFreis <ricreis71@gmail.com>
2021-12-16 18:50:12 -0300

    Testes Unitários do cliente 16/12/2021

---
+++ TestProject/ClienteTest.cs
@@ -0,0 +1,44 @@
+using Boteco32.Controllers;
+using Boteco32.Models;
+using Boteco32.Repository;
+using Boteco32.Services;
+using Boteco32.ViewModels.ClienteViewModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestProject
+{
+    [TestClass]
+    public class ClienteTest
+    {
+        ClientesController _clientesController = new ClientesController();
+
+        [TestMethod]
+        public void BuscaTodosClientes()
+        {
+            var cli = _clientesController.GetClientes();
+            Assert.AreNotEqual(null, cli);
+        }
+
+        [TestMethod]
+        public void BuscaClientePorId()
+        {
+            var cli = _clientesController.GetCliente(1);
+            Assert.AreNotEqual(null, cli.Id);
+        }
+
+        [TestMethod]
+        public void PostarNovoCliente()
+        {
+            Cliente c = new Cliente();
+            c.Nome= "Ricardo";
+            c.Email = "ricardo@ig.com.br";
+            c.Senha = "567";
+            c.Endereco = "Rua Sampaio Corrêa, 322";
+            c.Telefone = "(11) 2239-4059";
+
+            var cl = _clientesController.PostNovoCliente(c);
+            Assert.AreNotEqual(null, cl);
+        }
+    }
+}
